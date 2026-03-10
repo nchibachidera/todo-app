b1174ca2886d56d3683bbc73e5c53d44cfc68308
N.DERAJ <nchibachidera0@gmail.com>
2026-03-10 17:36:40 +0100

    first commit

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+import express from "express"
+import cors from "cors"
+import pool from './db.js'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => { 
+    res.send("server is running")
+})
+
+app.get("/test-db", async (req, res) => {
+
+    try {
+        const result = await pool.query("SELECT NOW()")
+        res.json({ message: 'Database connected', time: result.rows[0].now })
+    } catch (error) {
+        res.json({ error: error.message })
+    }
+})
+
+app.listen(3000, () => {
+    console.log("server running on port 3000")
+})
