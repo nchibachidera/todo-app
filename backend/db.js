9e3812ef169aa362fb6a6cb1d895d140538f5c1d
N.DERAJ <nchibachidera0@gmail.com>
2026-03-11 08:32:56 +0100

    updated project with latest changes

--- backend/db.js
+++ backend/db.js
@@ -2,6 +2,7 @@ import pg from 'pg'
 import dotenv from 'dotenv'
 
 dotenv.config()
+console.log(process.env.DB_USER, process.env.DB_HOST, process.env.DB_NAME, process.env.DB_PORT)
 
 const { Pool } = pg
 
