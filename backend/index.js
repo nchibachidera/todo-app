4afb6c248bdecd341a83c42a7846d9d7b925a386
N.DERAJ <nchibachidera0@gmail.com>
2026-03-11 08:03:36 +0100

    updated project with latest changes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express"
 import cors from "cors"
 import pool from './db.js'
+import router from "./routes.js"
+app.use("/api", router)
 
 const app = express()
 
---
+++ backend/routes.js
@@ -0,0 +1,49 @@
+import { Router } from "express"
+import pool from "./db.js"
+
+const router = Router()
+
+//get all todos
+router.get("/todos", async (req, res) => {
+    try{
+        const result = await pool.query("SELECT * FROM todos ORDER BY created_at DESC")
+        res.json(result.rows)
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+})
+
+//create a todo
+router.post("/todos", async (req, res) => {
+    try {
+        const { title } = req.body
+        const result = await pool.query("INSERT INTO todos (title) VALUES ($1) RETURNING *", [title])
+        res.json(result.rows[0])
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+})
+
+//update a todo 
+router.put("/todos/:id", async (req, res) => {
+    try {
+        const { id } = req.params
+        const result = await pool.query("UPDATE todos SET is_completed = Not is_completed WHERE id = $1 RETURNING *", [id])
+        res.json(result.rows[0])
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+})
+
+//delete a todo
+router.delete ("/todos/:id", async (req, res) => {
+    try {
+        const { id } = req.params
+        await pool.query("DELETE FROM todos WHERE id = $1", [id])
+        res.json({ message: "Todo deleted" })
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+})
+
+export default router
