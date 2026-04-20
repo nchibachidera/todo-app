7ead926913d2f29313a3535901a22892f501214c
N.DERAJ <nchibachidera0@gmail.com>
2026-04-20 09:14:22 +0100

    updated project with latest changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,39 @@
-import { useState, useEffect } from 'react';
-
+import { useState, useEffect } from 'react'
 
 function App() {
   const [todos, setTodos] = useState([])
-  const [titles, setTitles] = useState('')
-};
+  const [title, setTitle] = useState('')
 
-
-useEffect(() => {
-  fetchTodos()
+  useEffect(() => {
+    fetchTodos()
   }, [])
 
   const fetchTodos = async () => {
     const response = await fetch('http://localhost:3000/api/todos')
     const data = await response.json()
     setTodos(data)
-  };
-  
+  }
+
   const createTodo = async () => {
     if (!title) return
     await fetch('http://localhost:3000/api/todos', {
       method: 'POST',
-      headers: { 'Conent-Type': 'Application/json' },
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ title })
     })
     setTitle('')
     fetchTodos()
   }
 
-
   const toggleTodo = async (id) => {
-    await fetch('http://localhost:3000/api/todos/${id}', {
+    await fetch(`http://localhost:3000/api/todos/${id}`, {
       method: 'PUT'
     })
     fetchTodos()
   }
 
   const deleteTodo = async (id) => {
-    await fetch('http://localhost:3000/api/todos/${id}', {
+    await fetch(`http://localhost:3000/api/todos/${id}`, {
       method: 'DELETE'
     })
     fetchTodos()
@@ -47,19 +43,19 @@ useEffect(() => {
     <div>
       <h1>Todo App</h1>
       <input
-      type="text"
-      value={title}
-      onChange={(e) => setTitle(e.target.value)}
-      placeholder="Enter a todo"
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Enter a todo"
       />
-      <button onclick={createTodo}>Add</button>
+      <button onClick={createTodo}>Add</button>
 
       <ul>
-        {todos.map((todo) =>(
+        {todos.map((todo) => (
           <li key={todo.id}>
             <span
-            onClick={() => toggleTodo(todo.id)}
-            style={{ textDecoration: todo.is_completed ? 'line through' : 'none', cursor: 'pointer' }}
+              onClick={() => toggleTodo(todo.id)}
+              style={{ textDecoration: todo.is_completed ? 'line-through' : 'none', cursor: 'pointer' }}
             >
               {todo.title}
             </span>
@@ -69,5 +65,6 @@ useEffect(() => {
       </ul>
     </div>
   )
+}
 
-export default Apptodo
+export default App
