e8d2f95b9469025abc1d39080b9d135e96989d82
N.DERAJ <nchibachidera0@gmail.com>
2026-06-28 18:39:24 +0100

    updated project with latest changes

--- backend/authRoutes.js
+++ backend/authRoutes.js
@@ -16,6 +16,26 @@ router.post("/register", async (req, res) => {
         )
         res.json(result.rows[0])
     } catch (error) {
-        res.status(500).json({ error: error.message})
+        res.status(500).json({ error: error.message })
     }
 })
+
+
+
+//login
+router.post("/login", async (req, res) => {
+    try {
+        const {email, password} = req.body
+        const result = await pool.query("SELECT * FROM users WHERE email = $1", [email])
+        const user = result.rows[0]
+        if (!user) return res.status(400).json({ error: "user not found" })
+            const validPassword = await bcrypt.compare(password, user.password)
+        if (!validPassword) return res.status(400).json({ error: "wrong password" })
+            const token = jwt.sign({ userId: user.id}, process.env.JWT_SECRET)
+        res.json({ token })
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+})
+
+export default router
