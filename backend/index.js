ba4888ac598738e776ac67a5d3a28dd960bedf6c
N.DERAJ <nchibachidera0@gmail.com>
2026-06-29 19:03:49 +0100

    updated project with latest changes

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,9 @@ import authRouter from './authRoutes.js'
 const app = express()
 
 app.use(cors())
-app.use('/api/auth', authRouter)
 app.use(express.json())
 app.use("/api", router)
+app.use('/api/auth', authRouter)
 
 app.get("/", (req, res) => { 
     res.send("server is running")
