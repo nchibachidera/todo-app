28fd598bcc73c1e1da21291b33893451ab32670d
N.DERAJ <nchibachidera0@gmail.com>
2026-07-02 21:43:29 +0100

    updated project with latest changes

--- backend/authMiddleware.js
+++ backend/authMiddleware.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken"
 
 const authMidleware = (req, res, next) => {
     try {
-        const token = req.headers.authorization?.slit('')[1]
+        const token = req.headers.authorization?.split(' ')[1]
         if (!token) return res.status(401).json ({ error: "No token provided" })
             const decoded = jwt.verify(token, process.env.JWT_SECRET)
         req.userId = decoded.userId
