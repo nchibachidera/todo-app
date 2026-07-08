70e321c01d516616e3ad2873d0d9bed34fafec4d
N.DERAJ <nchibachidera0@gmail.com>
2026-07-08 04:59:52 +0100

    updated project with latest changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter, Routes, Route} from "react-router-dom"
 import App from './App.jsx'
+import Login from "./Login.jsx"
+import Register from "./Register.jsx"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/Login" element={<Login/>} />
+      <Route path="Register" element={<Register/>} />
+    </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
